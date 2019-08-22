d39ca273989ec622d2e648c8f9f8bb4322ca3173
diegofontecilla <mail@diegofontecilla.com>
2019-08-22 12:41:39 +0000

    added echo on testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -5,3 +5,4 @@ echo "$NUMBER"
 
 echo "this was run automagically and programmed by a human been that practise meditation"
 echo "another change in the source code 1, 2"
+echo "well done, this is working"
