f2429739463e309a3af2d6e83eae83023614c3c9
Diego Fontecilla <31539191+diegofontecilla@users.noreply.github.com>
2019-08-22 12:23:01 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
 echo "this was run automagically and programmed by a human been that practise meditation"
-echo "another change in the source code"
+echo "another change in the source code 1"
